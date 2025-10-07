f15f945b1ca8a64d598ce3a6574be68428e35f95
Minarox <contact@minarox.fr>
2025-10-07 10:20:59 +0200

    feat: Auto import Inter font

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,15 +1,16 @@
-// @ts-check
-import { defineConfig } from 'astro/config';
-import sitemap from '@astrojs/sitemap';
-import metaTags from 'astro-meta-tags';
-import icon from 'astro-icon';
-import compressor from 'astro-compressor';
-import cloudflare from "@astrojs/cloudflare";
-
-import vue from '@astrojs/vue';
+import { defineConfig } from "astro/config"
+import sitemap from "@astrojs/sitemap"
+import metaTags from "astro-meta-tags"
+import icon from "astro-icon"
+import { astroFont } from "astro-font/integration"
+import vue from "@astrojs/vue"
+import compressor from "astro-compressor"
+import cloudflare from "@astrojs/cloudflare"
 
 // https://astro.build/config
 export default defineConfig({
+    site: "https://racecast.minarox.fr",
+    integrations: [sitemap(), metaTags(), icon(), astroFont(), vue(), compressor()],
     adapter: cloudflare({
         imageService: "cloudflare",
         platformProxy: {
