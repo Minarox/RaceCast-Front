ae004c1ed1f3779b1a56de04e4f432afeee5c539
Minarox <contact@minarox.fr>
2025-11-26 16:54:26 +0100

    fix: SSR on Cloudflare

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,7 @@
 import { defineConfig } from "astro/config"
+import icon from "astro-icon"
 import sitemap from "@astrojs/sitemap"
 import metaTags from "astro-meta-tags"
-import icon from "astro-icon"
 import { astroFont } from "astro-font/integration"
 import vue from "@astrojs/vue"
 import compressor from "astro-compressor"
@@ -10,16 +10,17 @@ import cloudflare from "@astrojs/cloudflare"
 // https://astro.build/config
 export default defineConfig({
     site: "https://racecast.minarox.fr",
-    integrations: [sitemap(), metaTags(), icon(), astroFont(), vue(), compressor()],
+    base: '/',
+    integrations: [icon(), sitemap(), metaTags(), astroFont(), vue(), compressor()],
     adapter: cloudflare({
         imageService: "cloudflare",
-        platformProxy: {
-            enabled: true
-        }
+		platformProxy: {
+			enabled: true
+		}
     }),
     vite: {
         ssr: {
-          external: ["buffer", "path", "fs", "os", "crypto", "async_hooks"].map((i) => `node:${i}`)
+          external: ["path", "fs"].map((i) => `node:${i}`)
         }
     }
 })
